e2ab5b7802fd2937a36ce5c40f59f8b21885d44a
TealNerd <tealnerd@gmail.com>
2017-12-10 12:23:59 -0600

    Add archetype files

---
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__.java
@@ -0,0 +1,14 @@
+package ${groupId};
+
+import vg.civcraft.mc.civmodcore.ACivMod;
+
+public class ${artifactId} extends ACivMod {
+	
+	public void onEnable() {
+		
+	}
+	
+	public String getPluginName() {
+		return "${artifactId}";
+	}
+}
